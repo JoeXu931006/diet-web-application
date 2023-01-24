bfc4eb626e910185e9cc5572d62294d276d046b7
joe19931006@gmail.com <joe19931006@gmail.com>
2023-01-24 14:55:52 +1100

    sign up and login page

--- resume-management/src/App.tsx
+++ resume-management/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LogInPage from "./pages/LogInPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LogInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </Router>
   );
